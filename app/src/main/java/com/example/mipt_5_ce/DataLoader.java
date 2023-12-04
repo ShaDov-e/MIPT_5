76e907679c78d7b1338fd9f921ee204f7eb67f6d
Sarunas <sarunasdov4@gmail.com>
2023-12-05 01:58:02 +0200

    changes

--- app/src/main/java/com/example/mipt_5_ce/DataLoader.java
+++ app/src/main/java/com/example/mipt_5_ce/DataLoader.java
@@ -1,6 +1,5 @@
 package com.example.mipt_5_ce;
 
-
 import android.os.AsyncTask;
 import android.util.Log;
 import org.w3c.dom.Document;
@@ -14,65 +13,110 @@ import java.util.List;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
-public class DataLoader extends AsyncTask<String, Void, List<String>> {
+public class DataLoader extends AsyncTask<String, Void, Void> {
+
+    private List<String> currencyOptions;
+    private String convertedCurrency;
 
     @Override
-    protected List<String> doInBackground(String... params) {
-        String selectedCurrency = params[0];
-        List<String> data = new ArrayList<>();
+    protected Void doInBackground(String... params) {
+        if (params.length > 0) {
+            // Fetch initial currency options
+            fetchCurrencyOptions();
 
+            // Fetch exchange rates and perform conversion
+            if (params.length > 2) {
+                String sourceCurrency = params[0];
+                String targetCurrency = params[1];
+                double amount = Double.parseDouble(params[2]);
+
+                fetchExchangeRates(sourceCurrency, targetCurrency, amount);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void result) {
+        // Update UI with fetched data
+        if (currencyOptions != null) {
+            MainActivity.getInstance().updateCurrencyOptions(currencyOptions);
+        }
+
+        if (convertedCurrency != null) {
+            MainActivity.getInstance().updateConvertedCurrency(convertedCurrency);
+        }
+    }
+
+    private void fetchCurrencyOptions() {
         try {
-            // Construct the URL
             URL url = new URL("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-            // Open connection
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestMethod("GET");
-            InputStream in = urlConnection.getInputStream();
-
-            // Parse XML
+            InputStream inputStream = connection.getInputStream();
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(in);
-            doc.getDocumentElement().normalize();
+            Document doc = dBuilder.parse(inputStream);
 
-            // Find the Cube containing the rates
-            NodeList cubeList = doc.getElementsByTagName("Cube");
+            Element rootElement = doc.getDocumentElement();
+            NodeList cubeList = rootElement.getElementsByTagName("Cube");
+
+            currencyOptions = new ArrayList<>();
+
             for (int i = 0; i < cubeList.getLength(); i++) {
-                Element cubeElement = (Element) cubeList.item(i);
+                Element cube = (Element) cubeList.item(i);
+                if (cube.hasAttribute("currency")) {
+                    currencyOptions.add(cube.getAttribute("currency"));
+                }
+            }
 
-                if (cubeElement.hasAttribute("time")) {
-                    // This is the Cube containing rates
-                    NodeList ratesList = cubeElement.getElementsByTagName("Cube");
-                    for (int j = 0; j < ratesList.getLength(); j++) {
-                        Element rateElement = (Element) ratesList.item(j);
+            inputStream.close();
+            connection.disconnect();
 
-                        String currencyCode = rateElement.getAttribute("currency");
-                        String rate = rateElement.getAttribute("rate");
+        } catch (Exception e) {
+            Log.e("DataLoader", "Error fetching currency options: " + e.getMessage());
+        }
+    }
 
-                        // Filter by selected currency
-                        if (selectedCurrency.isEmpty() || currencyCode.equals(selectedCurrency)) {
-                            data.add(currencyCode + " - " + rate);
-                        }
+    private void fetchExchangeRates(String sourceCurrency, String targetCurrency, double amount) {
+        try {
+            URL url = new URL("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            InputStream inputStream = connection.getInputStream();
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputStream);
+
+            Element rootElement = doc.getDocumentElement();
+            NodeList cubeList = rootElement.getElementsByTagName("Cube");
+
+            double sourceRate = 1.0;
+            double targetRate = 1.0;
+
+            for (int i = 0; i < cubeList.getLength(); i++) {
+                Element cube = (Element) cubeList.item(i);
+                if (cube.hasAttribute("currency") && cube.hasAttribute("rate")) {
+                    String currency = cube.getAttribute("currency");
+                    double rate = Double.parseDouble(cube.getAttribute("rate"));
+
+                    if (currency.equals(sourceCurrency)) {
+                        sourceRate = rate;
+                    } else if (currency.equals(targetCurrency)) {
+                        targetRate = rate;
                     }
                 }
             }
 
-            // Close connection
-            in.close();
-            urlConnection.disconnect();
+            double convertedAmount = (amount / sourceRate) * targetRate;
+            convertedCurrency = String.format("%.2f %s", convertedAmount, targetCurrency);
+
+            inputStream.close();
+            connection.disconnect();
+
         } catch (Exception e) {
-            Log.e("DataLoader", "Error fetching data: " + e.getMessage());
-        }
-
-        return data;
-    }
-
-    @Override
-    protected void onPostExecute(List<String> result) {
-        MainActivity mainActivity = MainActivity.getInstance();
-        if (mainActivity != null) {
-            mainActivity.updateUI(result);
+            Log.e("DataLoader", "Error fetching exchange rates: " + e.getMessage());
         }
     }
 }
+
--- app/src/main/java/com/example/mipt_5_ce/MainActivity.java
+++ app/src/main/java/com/example/mipt_5_ce/MainActivity.java
@@ -5,56 +5,90 @@ import android.os.AsyncTask;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static MainActivity instance;
-    private ListView listView;
-    private EditText filterEditText;
+        private static MainActivity instance;
+        private EditText amountEditText;
+        private Spinner sourceCurrencySpinner;
+        private Spinner targetCurrencySpinner;
+        private TextView convertedTextView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
 
-        instance = this;
-        listView = findViewById(R.id.listView);
-        filterEditText = findViewById(R.id.filterEditText);
+                instance = this;
+                amountEditText = findViewById(R.id.amountEditText);
+                sourceCurrencySpinner = findViewById(R.id.sourceCurrencySpinner);
+                targetCurrencySpinner = findViewById(R.id.targetCurrencySpinner);
+                convertedTextView = findViewById(R.id.convertedTextView);
 
-        // Set up the adapter and set it to the ListView
-        List<String> exchangeRates = new ArrayList<>();
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, exchangeRates);
-        listView.setAdapter(adapter);
-    }
+                // Populate the source and target currency Spinners with default values
+                List<String> defaultCurrencies = new ArrayList<>();
+                defaultCurrencies.add("USD");
+                defaultCurrencies.add("EUR");
 
-    public static MainActivity getInstance() {
-        return instance;
-    }
+                ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, defaultCurrencies);
+                spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-    // Called when the user clicks the "Load Data" button
-    public void loadData(View view) {
-        // Get the selected currency code from the EditText or Dropdown item
-        String selectedCurrency = filterEditText.getText().toString();
+                sourceCurrencySpinner.setAdapter(spinnerAdapter);
+                targetCurrencySpinner.setAdapter(spinnerAdapter);
 
-        if (!selectedCurrency.isEmpty()) {
-            // Execute DataLoader AsyncTask to fetch data
-            new DataLoader().execute(selectedCurrency);
-        } else {
-            Toast.makeText(this, "Enter a currency code", Toast.LENGTH_SHORT).show();
+                // Execute DataLoader AsyncTask to fetch currency options
+                new DataLoader().execute();
         }
-    }
 
-    // Method to update the UI with the fetched data
-    public void updateUI(List<String> data) {
-        ArrayAdapter<String> adapter = (ArrayAdapter<String>) listView.getAdapter();
-        adapter.clear();
-        adapter.addAll(data);
-    }
+        public static MainActivity getInstance() {
+                return instance;
+        }
+
+        // Called when the user clicks the "Convert" button
+        public void convertCurrency(View view) {
+                // Get the selected source and target currencies
+                String sourceCurrency = (String) sourceCurrencySpinner.getSelectedItem();
+                String targetCurrency = (String) targetCurrencySpinner.getSelectedItem();
+
+                if (sourceCurrency != null && targetCurrency != null) {
+                        // Execute DataLoader AsyncTask to fetch exchange rates and perform conversion
+                        String amountString = amountEditText.getText().toString();
+                        if (!amountString.isEmpty()) {
+                                double amount = Double.parseDouble(amountString);
+                                new DataLoader().execute(sourceCurrency, targetCurrency, String.valueOf(amount));
+                        } else {
+                                Toast.makeText(this, "Enter an amount", Toast.LENGTH_SHORT).show();
+                        }
+                } else {
+                        Toast.makeText(this, "Select source and target currencies", Toast.LENGTH_SHORT).show();
+                }
+        }
+
+        // Method to update the UI with the fetched data
+        public void updateCurrencyOptions(List<String> currencyOptions) {
+                ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, currencyOptions);
+                spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                sourceCurrencySpinner.setAdapter(spinnerAdapter);
+                targetCurrencySpinner.setAdapter(spinnerAdapter);
+        }
+
+        // Method to update the UI with the converted currency
+        public void updateConvertedCurrency(String convertedCurrency) {
+                convertedTextView.setText("Converted Currency: " + convertedCurrency);
+        }
 }
+
+
+
 
